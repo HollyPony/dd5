2e4ef33318cad27742dbd24470c2e441a4b3f838
Parapapatte <arthur.agombart@gmail.com>
2026-02-03 21:52:28 +0100

    factorize errors

--- modules/errors.js
+++ modules/errors.js
@@ -1,49 +1,46 @@
 import { showErrorToast } from './toast.js'
 
-export function InvalidClassNameError(className, ...props) {
-  const message = `Invalid Class '${className}'`
-  const err = new Error(message, ...props)
-  Object.setPrototypeOf(err, InvalidClassNameError.prototype)
-  err.name = "InvalidClassNameError"
-  err.className = className
+function createCustomError({ name, message, data = {}, args = [] }) {
+  const err = new Error(message, ...args)
+  Object.setPrototypeOf(err, createCustomError.prototype)
+  err.name = name
+  Object.assign(err, data)
   showErrorToast(err)
-
   return err
 }
-InvalidClassNameError.prototype = Object.create(Error.prototype, { constructor: { value: InvalidClassNameError, } })
+createCustomError.prototype = Object.create(Error.prototype, { constructor: { value: createCustomError, } })
+
+export function InvalidClassNameError(className, ...props) {
+  return createCustomError({
+    name: 'InvalidClassNameError',
+    message: `Invalid Class '${className}'`,
+    data: { className },
+    args: props,
+  })
+}
 
 export function InvalidSubClassNameError(subClassName, className, ...props) {
-  const message = `Invalid SubClass '${subClassName}' for '${className}'`
-  const err = new Error(message, ...props)
-  Object.setPrototypeOf(err, InvalidSubClassNameError.prototype)
-  err.name = "InvalidSubClassNameError"
-  err.className = className
-  err.subClassName = subClassName
-  showErrorToast(err)
-
-  return err
+  return createCustomError({
+    name: 'InvalidSubClassNameError',
+    message: `Invalid SubClass '${subClassName}' for '${className}'`,
+    data: { className, subClassName },
+    args: props,
+  })
 }
-InvalidSubClassNameError.prototype = Object.create(Error.prototype, { constructor: { value: InvalidSubClassNameError, } })
 
 export function BadDiceError(dice, ...props) {
-  const message = `Dice '${dice}' is not does not exist`
-  const err = new Error(message, ...props)
-  Object.setPrototypeOf(err, BadDiceError.prototype)
-  err.name = "BadDiceError"
-  err.dice = dice
-  showErrorToast(err)
-
-  return err
+  return createCustomError({
+    name: 'BadDiceError',
+    message: `Dice '${dice}' is not does not exist`,
+    data: { dice },
+    args: props,
+  })
 }
-BadDiceError.prototype = Object.create(Error.prototype, { constructor: { value: MissingPathError, } })
 
 export function MissingPathError(...props) {
-  const message = `A 'path' is required`
-  const err = new Error(message, ...props)
-  Object.setPrototypeOf(err, MissingPathError.prototype)
-  err.name = "MissingPathError"
-  showErrorToast(err)
-
-  return err
+  return createCustomError({
+    name: 'MissingPathError',
+    message: `A 'path' is required`,
+    args: props,
+  })
 }
-MissingPathError.prototype = Object.create(Error.prototype, { constructor: { value: MissingPathError, } })
