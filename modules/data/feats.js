2f46e640611d73a2a2c421a6399528d750d6c1a4
Parapapatte <arthur.agombart@gmail.com>
2026-02-04 20:24:47 +0100

    feat: define FEAT_CATEGORY and update alert category to use it

--- modules/data/feats.js
+++ modules/data/feats.js
@@ -1,10 +1,13 @@
 // P.200
 import { f, } from '../helpers.js'
 
+const FEAT_CATEGORY = f({
+  GENERAL: Symbol('feat_category_general'),
+})
+
 export const feats = f({ // TODO:
   alert: f({
-    category: 'general',
-    description: 'Always on the lookout for danger, you gain the following benefits: You gain a +5 bonus to initiative. You can\'t be surprised while you are conscious. Other creatures don\'t gain advantage on attack rolls against you as a result of being unseen by you.',
+    category: FEAT_CATEGORY.GENERAL,
     benefits: f({
       initiativeBonus: 5,
       noSurprise: true,
