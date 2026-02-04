9ae8421124ae43dfdf052ded2141d588a9d50507
Parapapatte <arthur.agombart@gmail.com>
2026-02-04 14:22:39 +0100

    refactor: remove unused import of removeAllChildren from domlib for cleaner code

--- components/Ability/index.js
+++ components/Ability/index.js
@@ -1,6 +1,6 @@
 import { AbstractComponent } from '../AbstractComponent/index.js'
 import charSheet from '../../modules/stores/charSheet.store.js'
-import { createElement, fillElement, removeAllChildren } from '../../modules/domlib.js'
+import { createElement, fillElement, } from '../../modules/domlib.js'
 import { ABILITY, SKILLS, } from '../../modules/common.js'
 import { domSubscribe, signDisplay, } from '../../modules/helpers.js'
 import { t } from '../../modules/i18n.js'
