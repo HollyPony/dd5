75dc73abdd1ef5eded58bc5732260f3e79397142
Parapapatte <arthur.agombart@gmail.com>
2026-02-17 17:23:03 +0100

    refactor(initApp): streamline event registration and translation updates

--- index.js
+++ index.js
@@ -4,43 +4,27 @@ import charSheetService from './modules/services/charSheet.service.js'
 import { domSubscribe } from './modules/domlib.js'
 import registerWebComponents from './index.webmodules.js'
 
-const translations = document.querySelectorAll('[data-i18n], [data-i18n-attributes]')
-const subscriptions = []
-
-function translationsChanged() {
-  for (const element of translations) {
-    i18n.applyTranslation(element)
-  }
-}
-
-function pageHided() {
-  unregisterSubscriptions()
-}
-
-function registerDomEvents() {
-  subscriptions.push(
-    domSubscribe(window, 'pagehide', pageHided),
-  )
-}
-
-function registerTranslationsUpdates() {
-  subscriptions.push(
-    i18n.subscribe(() => translationsChanged()),
-  )
-}
-
-function unregisterSubscriptions() {
-  while (subscriptions.length) subscriptions.pop()?.()
-  charSheetService.unregister()
-}
 
 async function initApp() {
+  const subscriptions = []
+  const translations = document.querySelectorAll('[data-i18n], [data-i18n-attributes]')
+
   charSheetService.init()
 
-  registerDomEvents()
-  registerTranslationsUpdates()
-  registerWebComponents()
 
+  subscriptions.push(
+    domSubscribe(window, 'pagehide', function unregisterSubscriptions() {
+      while (subscriptions.length) subscriptions.pop()?.()
+      charSheetService.unregister()
+    }),
+    i18n.subscribe(() => {
+      for (const element of translations) {
+        i18n.applyTranslation(element)
+      }
+    }),
+  )
+
+  registerWebComponents()
   initTranslations()
 }
 
